2944d7ce8687742f7f70ab0085f1328466b61df5
zhengtaotao <zhengtaotao@jd.com>
2018-01-26 19:07:43 +0800

    joda

---
+++ java-basic/src/main/java/com/tt/jodatime/JodaPractice.java
@@ -0,0 +1,21 @@
+package com.tt.jodatime;/**
+ * Created by zhengtaotao on 2018/1/26.
+ */
+
+import java.time.LocalDate;
+
+/**
+ * java8 时间api
+ * User: zhengtaotao
+ * Date: 2018/1/26
+ */
+public class JodaPractice {
+
+    public static void main(String[] args) {
+        // LocalDate LocalTime
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate.getYear());
+        System.out.println(localDate.getYear());
+    }
+
+}
